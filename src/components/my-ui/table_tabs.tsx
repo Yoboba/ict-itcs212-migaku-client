5fbfffa3a0e2e7aed308b7f33276713543310114
DEVN-P <pl.devn@outlook.com>
2024-03-16 01:25:06 +0700

    feat: add custom screensize + fix dialog responsiveness

--- src/components/my-ui/table_tabs.tsx
+++ src/components/my-ui/table_tabs.tsx
@@ -3,7 +3,7 @@ import CourseCard from './course_card'
 import TableSearchBar from './table_search_bar'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from '../ui/button'
-import { IconPlus, IconSortAscending2, IconUserPlus } from '@tabler/icons-react'
+import { IconCurrencyBaht, IconPlus, IconSortAscending2, IconUserPlus } from '@tabler/icons-react'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
@@ -24,6 +24,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
 
 let mock_data = [
     {
@@ -89,27 +90,70 @@ export default function TableTabs() {
                         <div className='hidden md:block'>Add Course</div>
                       </Button>
                     </DialogTrigger>
-                    <DialogContent className="sm:max-w-[800px]">
+                    <DialogContent className="nowrap:max-w-[800px] ">
                       <DialogHeader>
-                        <DialogTitle>Create a new course</DialogTitle>
+                        <DialogTitle>Course initialization</DialogTitle>
                         <DialogDescription>
                           Please fill in the required information marked with an asterisk (<span className='text-red-500'>*</span>).
                         </DialogDescription>
                       </DialogHeader>
-                      <div id="dialog-content" className='flex gap-6'>
-                        <div id="image-upload-container">
-                          <div id="image-upload" className='min-w-[370px] h-[180px] border rounded-md'>
-
+                      <div id="dialog-content" className='flex gap-6 flex-col nowrap:flex-row'>
+                        <div id="container-left" className='flex-col space-y-4'>
+                          <div id="course-name-form" className="grid w-full gap-1.5">
+                            <Label htmlFor="name" className="">Course Name <span className='text-red-500'>*</span></Label>
+                            <Input id="name" placeholder='' className="col-span-3" />
+                          </div>
+                          <div id="image-upload" className='min-w-[370px] h-[180px] border-[2px] border-dashed rounded-md mt-4 cursor-pointer'>
                           </div>
                         </div>
-                        <div id="dialog-form" className='flex-col gap-2 w-full'>
-                          <Label htmlFor="name" className="">
-                            Course Name <span className='text-red-500'>*</span>
-                          </Label>
-                          <Input id="name" placeholder='' className="col-span-3 mt-1" />
+                        <div id="container-right" className='flex-col w-full space-y-4'>
+                          <div id="course-desc-form" className="grid w-full gap-1.5">
+                            <Label htmlFor="course-desc">Course Description</Label>
+                            <Textarea placeholder="Type your course description here. (Maximum 500 characters)" id="course-desc" />
+                          </div>
+                          <div className='flex gap-2'>
+                            <div id="course-category-form" className="grid gap-1.5 w-[60%]">
+                              <Label htmlFor="select-category" className="">Course Category <span className='text-red-500'>*</span></Label>
+                              <Select>
+                                <SelectTrigger className="w-full">
+                                  <SelectValue placeholder="Course Category" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                <SelectGroup>
+                                  <SelectItem value="IT">IT</SelectItem>
+                                  <SelectItem value="MA">Mathematics</SelectItem>
+                                  <SelectItem value="SC">Science</SelectItem>
+                                  <SelectItem value="LG">Languages</SelectItem>
+                                  <SelectItem value="SO">Social Sciences</SelectItem>
+                                </SelectGroup>
+                                </SelectContent>
+                              </Select>
+                            </div>
+                            <div id="course-status-form" className="grid w-full gap-1.5 flex-1">
+                              <Label htmlFor="select-status" className="">Course Status <span className='text-red-500'>*</span></Label>
+                              <Select>
+                                <SelectTrigger className="w-full">
+                                  <SelectValue placeholder="Status" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                  <SelectGroup>
+                                    <SelectItem value="0">Inactive</SelectItem>
+                                    <SelectItem value="1">Active</SelectItem>
+                                  </SelectGroup>
+                                </SelectContent>
+                              </Select>
+                            </div>
+                          </div>
+                          <div id="course-price-form" className="grid w-full gap-1.5">
+                            <Label htmlFor="course-price" className="">Course Price <b>(THB)</b> <span className='text-red-500'>*</span></Label>
+                            <div className='relative'>
+                              <IconCurrencyBaht size={24} className='absolute left-1 top-2'/>
+                              <Input id="price" placeholder='' className="col-span-3 pl-8"/>
+                            </div>      
+                          </div>
                         </div>
                       </div>
-                      <DialogFooter className='mt-4'>
+                      <DialogFooter className='mt-2'>
                         <Button type="submit">Save changes</Button>
                       </DialogFooter>
                     </DialogContent>
--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -18,6 +18,14 @@ const config = {
         "2xl": "1400px",
       },
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'nowrap': '840px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
