8efa0ca312bd61c6dad38334a09bc8c918bffc5a
DEVN-P <pl.devn@outlook.com>
2024-03-15 16:00:14 +0700

    feat: course category tooltips

--- src/components/my-ui/course_card.tsx
+++ src/components/my-ui/course_card.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
 
 type CourseProps = {
     course: {
@@ -10,6 +16,19 @@ type CourseProps = {
         imgSrc?: string,
     }
   }
+
+type CategoryMap = {
+    [key: string]: string;
+}
+
+const categoryMap: CategoryMap = {
+    "IT": "Information Technology",
+    "MA": "Mathematics",
+    "SC": "Science",
+    "LG": "Languages",
+    "SO": "Social Sciences",
+};
+
 const CourseCard = ({course}: CourseProps) => {
     if (!course.imgSrc) {
         course.imgSrc = "https://via.placeholder.com/180x180"
@@ -23,6 +42,19 @@ const CourseCard = ({course}: CourseProps) => {
                 layout="fill"
                 objectFit="cover"
             />
+        </div>
+        <div id="course-category" className="absolute top-1 left-[4px] font-semibold text-sm px-2 bg-slate-600 text-white rounded-[4px] cursor-pointer">
+            <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                    <TooltipTrigger asChild>
+                        <div className="">{course.courseCat}</div>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        {categoryMap[course.courseCat]}
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+            
         </div>
         <div id="card-footer" className='bg-slate-50 w-full py-2 px-4 flex absolute bottom-0'>
             <div id="course-name">
