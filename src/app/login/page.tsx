2dc729326a3dd1b82f550727d529c3978cc1cb73
KendoEon <rkhumsap@gmail.com>
2024-03-22 15:12:13 +0700

    Login Page but it's in the test stage.
    I did nothing with the page yet. It's just a simple form, but there's error. (pic in Discord)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
+import YouShallNotPass from "@/containers/Login-Page";
+
 export default function LoginPage() {
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            <p>LOGIN PAGE</p>
+            <YouShallNotPass/>
         </div>
     )
 }
---
+++ src/containers/Login-Page/Login.tsx
@@ -0,0 +1,21 @@
+import {FormEvent} from 'react'
+ 
+export default function YouShallNotPass() {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('/api/submit', {
+      method: 'POST',
+      body: formData,
+    })
+    const data = await response.json()
+  }
+ 
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
