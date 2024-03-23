bac141d046bb1fc460a94655bd97dbdaa677904f
S4NHXNU1 <sanhanut2003@gmail.com>
2024-03-23 15:43:54 +0700

    feat: WEB-48 add browse page layout

---
+++ src/app/browse/page.tsx
@@ -0,0 +1,9 @@
+import Browse from "@/containers/browse-page/browse-section";
+
+export default function BrowsePage() {
+    return (
+        <div className="w-full h-full flex">
+            <Browse/>
+        </div>
+    )
+}
