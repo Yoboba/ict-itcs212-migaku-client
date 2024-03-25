266fac99715836f28470561569d0bf50fe2c37f9
S4NHXNU1 <sanhanut2003@gmail.com>
2024-03-25 13:45:09 +0700

    fix: WEB-21 login layout alignment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,8 +4,14 @@ import MyFutureIsAVoid from "@/containers/Login-Page/BrightenYourFuture";
 export default function LoginPage() {
     return (
         <div className="w-full h-screen flex justify-center items-center">
-            <MyFutureIsAVoid/>
-            <YouShallNotPass/>
+            <div className="grid grid-cols-12 w-full mt-14">
+                <div className="col-span-6 flex justify-center items-center">
+                    <MyFutureIsAVoid/>
+                </div>
+                <div className="col-span-6 flex justify-center items-center">
+                    <YouShallNotPass/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/containers/Login-Page/Login.tsx
+++ src/containers/Login-Page/Login.tsx
@@ -1,49 +1,51 @@
+import { Button } from "@/components/ui/button";
+
 export default function YouShallNotPass() {
   return (
-    <main className="relative w-full xl:w-full h-screen top-0 right-0 flex flex-col items-start justify-start px-10 py-24 sm:px-20 sm:py-28 gap-8 overflow-hidden">
-    <div className="w-5/12 self-end">
-    <div className=" text-xl sm:text-3xl font-semibold leading-tight sm:leading-snug pb-4">
-                <h1>Log in to your account</h1>
-            </div>
-    <form className="bg-blue-100 rounded px-0 pt-0 pb-8 mb-4 focus:outline-none">
-      <div className="mb-4">
-        <label className="block text-gray-700 text-m mb-2" htmlFor="username">
-          Email
-        </label>
-        <input className="shadow appearance-none border rounded-lg w-96 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" />
-      </div>
-      <div className="mb-6">
-        <label className="block text-gray-700 text-m mb-2" htmlFor="password">
-          Password
-        </label>
-        <input className="shadow appearance-none border rounded-lg w-96 h-8 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password"/>
-        <p className="text-gray-400 text-sm text-left">Forgot Password?</p>
-      </div>
-      <div className="flex items-center justify-between">
-        <button className="bg-black text-white py-2 px-4  w-40 h-10 rounded-lg focus:outline-none focus:shadow-outline text-center content-center self-center place-self-center" type="button">
-          Log in
-        </button>
-      </div>
-      <div className=" text-xs sm:text-xs font-semibold leading-tight sm:leading-snug py-6">
-                <h1>————————————————or————————————————</h1>
-            </div>
-      <div className="mb-4">
-        <label className="block text-gray-700 text-m mb-2" htmlFor="username" >
-        </label>
-        <input className="shadow appearance-none border rounded-lg w-64 h-8 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Email"/>
-      </div>
-      <div className="mb-6">
-        <input className="shadow appearance-none border rounded-lg w-64 h-8 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"/>
-        <p className="text-gray-400 text-sm">New to us?</p>
-      </div>
-      <div className="flex items-center justify-between">
-        <button className="bg-black text-white py-2 px-4  w-40 h-10 rounded-lg focus:outline-none focus:shadow-outline text-center items-center" type="button">
-        Sign up
-        </button>
-      </div>
-      
-    </form>
-  </div>
-  </main>
+      <div className="">
+        <div className=" text-xl sm:text-3xl font-semibold leading-tight sm:leading-snug pb-4">
+            <h1>Log in to your account</h1>
+        </div>
+        <form>
+          <div>
+            <label className="block text-gray-700 text-m mb-2" htmlFor="username">
+              Email
+            </label>
+            <input className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" />
+          </div>
+          <div>
+            <label className="block text-gray-700 text-m mb-2 mt-2" htmlFor="password">
+              Password
+            </label>
+            <input className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password"/>
+            <p className="text-gray-400 text-sm text-right cursor-pointer hover:text-black">Forgot Password?</p>
+          </div>
+          <div className="my-4 flex justify-center">
+            <Button className="w-[50%] text-center" type="button">
+              Log in
+            </Button>
+          </div>
+          <div className="my-2 flex justify-center gap-2">
+            <div className="border-b-2 border-gray-400 w-[45%] mb-2"></div>
+            <p>or</p>
+            <div className="border-b-2 border-gray-400 w-[45%] mb-2"></div>
+          </div>
+          {/* <div className="mt-4">
+            <Button className="w-[100%] text-center" type="button">
+              Sign up
+            </Button>
+          </div> */}
+          {/* <div className="my-4">
+            <input className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Email"/>
+          </div>
+          <div className="mt-4">
+            <input className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password"/>
+          </div> */}
+          <div className="flex justify-center gap-2">
+            <p className="text-gray-400 text-sm">New to us?</p>
+            <p className="text-gray-400 cursor-pointer hover:text-black text-sm">Sign up</p>
+          </div>
+        </form>
+    </div>
   )
 }
