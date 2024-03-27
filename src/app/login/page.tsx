0b97d6a8a701d09c20f19e34c4c26f41319b59df
Yobubble <107536925+Yoboba@users.noreply.github.com>
2024-03-27 14:55:09 +0700

    fix : WEB-21 kendo's login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,11 @@
-import YouShallNotPass from "@/containers/Login-Page/Login";
-import MyFutureIsAVoid from "@/containers/Login-Page/BrightenYourFuture";
+import LoginSection from "@/containers/login-page/login-section";
+import ContentSection from "@/containers/login-page/content-section";
 
 export default function LoginPage() {
     return (
-        <div className="w-full h-screen flex justify-center items-center">
-            <div className="grid grid-cols-12 w-full mt-14">
-                <div className="col-span-6 flex justify-center items-center">
-                    <MyFutureIsAVoid/>
-                </div>
-                <div className="col-span-6 flex justify-center items-center">
-                    <YouShallNotPass/>
-                </div>
-            </div>
+        <div className="flex h-screen w-full">
+            <ContentSection/>
+            <LoginSection/>
         </div>
     )
 }
---
+++ src/containers/Login-Page/content-section/index.tsx
@@ -0,0 +1,15 @@
+export default function ContentSection() {
+    return (
+        
+        <div className="bg-c9 relative hidden h-screen w-1/2 items-center justify-center gap-5 overflow-hidden md:flex">
+            <div className="left-36 top-0 h-2/3 w-[5px] rounded-full bg-white"/>
+            <div className="text-7xl font-bold text-white">
+                <h1 className="my-2">Brighten</h1>
+                <h1 className="my-2">your</h1>
+                <h1 className="my-2">Future<br/></h1>
+            </div>
+            {/* <div className=" h-2/3 w-[25px] -rotate-45 bg-white absolute bottom-0 left-40 translate-y-28 rounded-full"/>
+            <div className= "w-1/5 h-full -rotate-45 bg-c8 absolute -bottom-[300px] -left-28"/> */}
+        </div>
+        )
+}
