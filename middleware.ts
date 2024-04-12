54ea3f643ea6e1188584be4f3d526fbf8b71c08f
Yobubble <107536925+Yoboba@users.noreply.github.com>
2024-04-12 21:10:29 +0700

    feat : WEB-0 check authorize

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { getCookie } from "@/utils/cookie";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const cookieUserId = getCookie("user_id");
+  const cookieUserRole = getCookie("user_role");
+  // login route
+  if (request.nextUrl.pathname.startsWith("/login")) {
+    if (cookieUserId) {
+      console.log("cookie existed");
+      return NextResponse.rewrite(new URL("/", request.url));
+    }
+    return NextResponse.next();
+  }
+  // manage route
+  if (request.nextUrl.pathname.startsWith("/manage")) {
+    if (cookieUserId && cookieUserRole) {
+      if (cookieUserRole.value !== "Teacher") {
+        console.log("role rejected");
+      }
+      return NextResponse.next();
+    }
+    console.log("cookie missing");
+    return NextResponse.rewrite(new URL("/", request.url));
+  }
+}
