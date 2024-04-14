03c10928bf1799e806947292d4e4d139161699cd
Yobubble <107536925+Yoboba@users.noreply.github.com>
2024-04-14 19:00:37 +0700

    feat : WEB-0 course detail dynamic route

---
+++ app/browse/[id]/page.tsx
@@ -0,0 +1,11 @@
+import CourseCardSection from "@/containers/detail-page/course-card-section";
+import CourseDescriptionSection from "@/containers/detail-page/course-description-section";
+
+export default function DetailPage({ params }: { params: { id: number } }) {
+  return (
+    <div>
+      <CourseCardSection courseId={params.id} />
+      <CourseDescriptionSection courseId={params.id} />
+    </div>
+  );
+}
---
+++ app/browse/api/route.tsx
@@ -0,0 +1,46 @@
+import { getCookie } from "@/utils/cookie";
+import { BaseResponse } from "@/utils/response";
+import console from "console";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const responseJson: BaseResponse = {
+    status: 0,
+    message: "",
+    data: null,
+  };
+  const courseId = request.nextUrl.searchParams.get("courseId");
+  const searchKey = request.nextUrl.searchParams.get("searchKey");
+  const courseCat = request.nextUrl.searchParams.get("courseCat");
+  const teacherName = request.nextUrl.searchParams.get("teacherName");
+
+  console.log("courseId = " + courseId);
+  console.log("searchKey = " + searchKey);
+  console.log("courseCat = " + courseCat);
+  console.log("teacherName = " + teacherName);
+
+  const cookieUserId = getCookie("user_id");
+  console.log("user_id = " + cookieUserId?.value);
+
+  const response = await fetch(
+    `http://localhost:3001/api/course?courseId=${courseId}&searchKey=&courseCat=${courseCat}&teacherName=`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `${cookieUserId?.value}`,
+      },
+    }
+  );
+  const data = await response.json();
+
+  if (response.ok) {
+    responseJson.status = 200;
+    responseJson.message = "get course successfully";
+    responseJson.data = data;
+  } else {
+    responseJson.status = 500;
+    responseJson.message = "get course failed";
+    responseJson.data = data;
+  }
+  return NextResponse.json(responseJson);
+}
