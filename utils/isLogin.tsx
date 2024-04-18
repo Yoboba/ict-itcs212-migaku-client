498d09efc1a5d1d78060d118f8c35e6f761b09fd
S4NHXNU1 <sanhanut2003@gmail.com>
2024-04-18 13:03:03 +0700

    fix: WEB-54 remove isLogin()

--- utils/isLogin.tsx
+++
@@ -1,7 +0,0 @@
-import getCookie from "./getCookie";
-
-export default function isLogin() {
-  const cookieMap = getCookie();
-  if (cookieMap === undefined || cookieMap.UserId === undefined) return false;
-  else return true;
-}
