e7baf98ba03de74df036d2966faec3df0f3c04c3
Yobubble <thanachot.onl@student.mahidol.edu>
2024-04-18 14:12:16 +0700

    chore : WEB-0 logout button

--- components/my-ui/log_out_button.tsx
+++ components/my-ui/log_out_button.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export function LogOutButton() {
   const router = useRouter();
   async function handleLogOut() {
-    const response = await fetch("/login/api", { method: "DELETE" });
+    const response = await fetch("/api/login", { method: "DELETE" });
     const data = await response.json();
     console.log(data);
     router.refresh();
